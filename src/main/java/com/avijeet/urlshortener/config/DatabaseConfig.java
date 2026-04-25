35f3c0fa15b1b220c1f6af625fc9d6760f1664ac
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-04-25 17:33:47 +0530

    Add: DB and redis config

---
+++ src/main/java/com/avijeet/urlshortener/config/DatabaseConfig.java
@@ -0,0 +1,11 @@
+package com.avijeet.urlshortener.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.avijeet.urlshortener.repository")
+public class DatabaseConfig {
+}
